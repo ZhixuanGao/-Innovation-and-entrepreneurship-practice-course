183658385a7ce983e81f5648d05af73695ac9bf0
Gange <86412052+ZhixuanGao@users.noreply.github.com>
2022-07-24 08:32:59 +0800

    Update SM3_PRE.h

--- SM3_PRE/SM3_PRE.h
+++ SM3_PRE/SM3_PRE.h
@@ -1,1 +1,16 @@
+#pragma once
 
+#define HASH_SIZE 32 //哈希向量的大小
+
+namespace SM3 {
+
+	typedef struct sm3_context_s {
+		
+		unsigned char MessageBlock[64]; //512位的数据块
+
+		unsigned int IntermediateHash[HASH_SIZE / 4];
+	}sm3_context_t;
+
+	unsigned char *Calculate(const unsigned char *message, unsigned int messageLen, unsigned char digest[HASH_SIZE]); //计算函数
+	std::vector<uint32_t> Implement_SM3(char *filepath); //执行sm3
+}
